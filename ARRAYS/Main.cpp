dc3f4138f25384337268c0cce70d893ae3f4b937
Ivlie_am02@top-academy.ru <Ivlie_am02@top-academy.ru>
2024-02-05 21:38:15 +0300

    Introduction to Arrays

--- ARRAYS/Main.cpp
+++ ARRAYS/Main.cpp
@@ -1,7 +1,27 @@
+//Arrays
 #include <iostream>
 using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Russian");
+	//cout << "Hello, Arrays!" << endl;
+	const int SIZE = 5;
+	int arr[SIZE] = { 3,5,8 };
+	//cout << int() << endl;
+	//arr[2] = 2048;
+	//cout << arr[2] << endl;
+	//ввод элементов массива с клавиатуры
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin>>i;
+		cout<< i << endl;
+	}
 
+
+	//вывод элементов массива на консоль
+	/*for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;*/
 }
